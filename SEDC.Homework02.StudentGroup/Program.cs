7f051da1ae0b1335e8230712575e524c96957dfa
Darin12m <darinmiladinovski@gmail.com>
2023-03-05 01:07:38 +0100

    Add project files.

---
+++ SEDC.Homework02.StudentGroup/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string[] studentsG1 = new string[5] { "Krste","Petko","Trajko","Milan","Stojan"};
+string[] studentsG2 = new string[5] { "Zdravko", "Darko", "Stanko", "Branko", "Andrej" };
+
+Console.WriteLine("Enter student group: (1 or 2): ");
+int groupNumber = 0;
+
+if (!int.TryParse(Console.ReadLine(), out groupNumber))
+{
+    Console.WriteLine("Please enter valid number");
+}
+else if(groupNumber == 1)
+{
+    Console.WriteLine("The students from G1 are : ");
+    foreach(string student in studentsG1)
+    {
+        Console.WriteLine(student);
+    }
+}else if(groupNumber == 2)
+{
+    Console.WriteLine("The students from G2 are: ");
+    foreach(string student in studentsG2)
+    {
+        Console.WriteLine(student);
+    }
+}
---
+++ SEDC.Homework02.SumOfEven/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int[] numbersArr = new int[6];
+
+
+for (int i = 0; i < numbersArr.Length; i++)
+{
+    Console.WriteLine("Enter 6 numbers: ");
+
+    if (!int.TryParse(Console.ReadLine(), out numbersArr[i])){
+        Console.WriteLine("Enter a Valid Number! ");
+    }
+
+
+    int sum = 0;
+    foreach(int number in numbersArr)
+    {
+        if (number%2==0) { 
+        sum += number;
+        }
+    }
+    Console.WriteLine("The result is:" + sum);
+}
